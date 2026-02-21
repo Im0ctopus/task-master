d5e422e96009d0ca9c29012e05dfea48f80ca8cd
Im0ctopus <leoferreira1712@gmail.com>
2026-02-21 16:42:48 +0000

    Add noTask screens

--- src/lib/components/tasks/taskList.svelte
+++ src/lib/components/tasks/taskList.svelte
@@ -1,22 +1,29 @@
 <script lang="ts">
+	import type { Status } from '$lib/types/status';
 	import type { Task } from '../../../routes/+page.svelte';
+	import NoTasks from './noTasks.svelte';
 	import TaskItem from './taskItem.svelte';
 
 	type Props = {
 		filteredTasks: Task[];
 		openedTasks: number[];
 		toggleTaskOpen: (id: number) => void;
+		selectedTab: Status;
 	};
 
-	let { filteredTasks, openedTasks, toggleTaskOpen }: Props = $props();
+	let { filteredTasks, openedTasks, toggleTaskOpen, selectedTab }: Props = $props();
 </script>
 
-<div class="relative max-h-full w-full overflow-y-auto">
-	<div class="mx-auto flex w-full max-w-4xl flex-col items-center justify-center gap-3 px-3 py-5">
-		{#each filteredTasks as task, index (task.id)}
-			<TaskItem {task} isOpen={openedTasks.includes(task.id)} {index} {toggleTaskOpen} />
-		{/each}
-	</div>
+<div class="relative h-full max-h-full w-full overflow-y-auto">
+	{#if !filteredTasks.length}
+		<NoTasks {selectedTab} />
+	{:else}
+		<div class="mx-auto flex w-full max-w-4xl flex-col items-center justify-center gap-3 px-3 py-5">
+			{#each filteredTasks as task, index (task.id)}
+				<TaskItem {task} isOpen={openedTasks.includes(task.id)} {index} {toggleTaskOpen} />
+			{/each}
+		</div>
+	{/if}
 </div>
 
 <!-- "scroll overlay" -->
