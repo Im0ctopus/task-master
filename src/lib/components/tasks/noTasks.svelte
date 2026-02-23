a50228a5d776e3c400dc3938d86eceb820d6abcc
Im0ctopus <leoferreira1712@gmail.com>
2026-02-23 20:13:11 +0000

    Update noTasks component message for clarity on task status

--- src/lib/components/tasks/noTasks.svelte
+++ src/lib/components/tasks/noTasks.svelte
@@ -13,8 +13,8 @@
 <div class="flex h-full w-full items-center justify-center font-semibold">
 	{#if selectedTab === 'none'}
 		<p class="px-10 text-center text-neutral-500">
-			You don't have any tasks yet! Press
-			<kbd class="rounded bg-neutral-700 px-1 py-0.5 text-neutral-400">Alt</kbd>
+			You don't have any working on tasks! Press
+			<kbd class="rounded bg-neutral-700 px-1 py-0.5 text-neutral-400">Ctrl</kbd>
 			+
 			<kbd class="rounded bg-neutral-700 px-1 py-0.5 text-neutral-400">Enter</kbd>
 			to jump to the input below and start planing
