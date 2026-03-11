e45e035af71635b03815abb4c1770c4145efcf90
Im0ctopus <leoferreira1712@gmail.com>
2026-03-11 22:52:07 +0000

    Add scrollToTask functionality to improve task navigation

--- src/lib/components/tasks/taskList.svelte
+++ src/lib/components/tasks/taskList.svelte
@@ -12,15 +12,43 @@
 	};
 
 	let { filteredTasks, openedTasks, toggleTaskOpen, selectedTab }: Props = $props();
+
+	let listRef: HTMLDivElement | null = $state(null);
+
+	const scrollToTask = (top: number, bottom: number) => {
+		// FIXME: Not working for subTasks
+		if (!listRef) return;
+
+		const listHeight = listRef.clientHeight;
+		const scrollTop = listRef.scrollTop;
+		const itemHeight = bottom - top;
+
+		if (
+			(scrollTop + 20 < top && scrollTop + listHeight - 20 > bottom) ||
+			(itemHeight > listHeight && scrollTop + 20 < top)
+		)
+			return;
+
+		listRef.scrollTo({
+			top: scrollTop > top ? top - 20 : top - listHeight + itemHeight + 20,
+			behavior: 'smooth'
+		});
+	};
 </script>
 
-<div class="relative h-full max-h-full w-full overflow-y-auto">
+<div bind:this={listRef} class="relative h-full max-h-full w-full overflow-y-auto">
 	{#if !filteredTasks.length}
 		<NoTasks {selectedTab} />
 	{:else}
 		<div class="mx-auto flex w-full max-w-4xl flex-col items-center justify-center gap-3 px-3 py-5">
 			{#each filteredTasks as task, index (task.id)}
-				<TaskItem {task} isOpen={openedTasks.includes(task.id)} {index} {toggleTaskOpen} />
+				<TaskItem
+					{task}
+					isOpen={openedTasks.includes(task.id)}
+					{index}
+					{toggleTaskOpen}
+					{scrollToTask}
+				/>
 			{/each}
 		</div>
 	{/if}
